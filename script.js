20be81c5eb322d54f6a8284dab20d50d0ea04fd2
David salem <davidsalem27@gmail.com>
2024-10-07 19:21:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/**
+ * function for the buttons in the hamburger menu
+ */
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+function aboutText(){
+  
+}
